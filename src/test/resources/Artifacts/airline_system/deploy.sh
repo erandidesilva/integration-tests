f974880986a58b9b12110cea873dee3dfa81463e
erandidesilva <jae@swear.com>
2017-10-06 15:02:07 +0530

    Add a full string for term acceptance

--- src/test/resources/Artifacts/airline_system/deploy.sh
+++ src/test/resources/Artifacts/airline_system/deploy.sh
@@ -22,7 +22,7 @@ echo "Kube Master URL is Set to : "$KUBERNETES_MASTER
 echo "Current location : "$script_path
 echo "Creating the K8S Pods!!!!"
 
-echo "Creating the MySQL RC and Service!"
+echo "Creating the MSSQL RC and Service!"
 #kubectl create -f $script_path/mysql_service.yaml
 kubectl create -f $script_path/mssql_rc.yaml
 #kubectl create -f $script_path/mysql_other_service.yaml
